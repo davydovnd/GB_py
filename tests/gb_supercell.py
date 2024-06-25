bd48a6808611719bc317adda46dc84d309f30f9f
Никита Давыдов <ndavydov@MacBook-Air-Nikita-2.local>
2024-06-25 17:24:56 +0300

    test gb_supercell.py is edited

--- tests/gb_supercell.py
+++ tests/gb_supercell.py
@@ -1,20 +1,18 @@
 from siman.geo import create_supercell
 from siman.calc_manage import smart_structure_read
 from Grain import supercell
-from Grain import grain
+from Grain.grain import grain_const
 
 r3m = smart_structure_read('lno.r3m.conventional.POSCAR')
 
 gr = grain_const(r3m, [-1, 1, -2], [-1, -1, 0])
 
 
-super_r3m = create_supercell(r3m, [[3, 0, 0], [0, 3, 0], [0, 0, 3]])
-super_r3m.printme()
-
-workinstance = supercell.tools(super_r3m)
-workinstance.threshold = 0.01
+workinstance = supercell.tools(gr)
+workinstance.threshold = 0.0001
 unitcell = workinstance.find_unitcell()
 
 print(f'unitcell natoms: {len(unitcell.keys())}')
 # for k, v in unitcell.items():
 #     print(k, v)
+gr.write_poscar('./POSCAR_gr')
